dfc97b4ab958270a87c78c84b62f19d267bdf200
nhopkins12 <62952979+nhopkins12@users.noreply.github.com>
2024-07-12 10:35:37 -0400

    agh2

--- boardmake.js
+++ boardmake.js
@@ -140,7 +140,7 @@ var {Spot, Good, Bad, Shop, Teleport, Player} = require("./objects")
                 }
             }
             // board.every(connectionNum) || 
-            if(count >= totalSpots*2 -1){
+            if(count >= (totalSpots*3)-1){
                 return
             }
             else{
