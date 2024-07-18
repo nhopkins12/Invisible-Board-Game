f23a272953e7f7167edad6e2962a093a522cf3d3
nhopkins12 <62952979+nhopkins12@users.noreply.github.com>
2024-07-18 16:20:09 -0400

    Test

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/reset', function(req, res){
             console.error(error);
             throw error;
         }});
-    res.redirect('/show')
+    res.redirect('../show')
 });
 
 app.get('/display', function (req, res){
