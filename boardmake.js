a2f8ef918a8dc3161fff6db510ff85f59ba608e9
nhopkins12 <62952979+nhopkins12@users.noreply.github.com>
2024-07-18 11:47:20 -0400

    agh

--- boardmake.js
+++ boardmake.js
@@ -270,7 +270,7 @@ var {Spot, Good, Bad, Shop, Teleport, Start, Player} = require("./objects")
         });
 
         start = board.filter((e) => e.connections >= 4 && e.type == "Bad" || e.type == "Good")
-        if (four.length == 0){
+        if (start.length == 0){
             start = board.filter((e) => e.connections >= 3 && e.type == "Bad" || e.type == "Good")
         }
         board[start[0].id-1] = new Start(start[0].id, start[0].x, start[0].y);
