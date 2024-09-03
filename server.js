bf2a320f62eb20025f782e09d28e09ac085e5f4f
nhopkins12 <62952979+nhopkins12@users.noreply.github.com>
2024-09-03 18:20:04 -0400

    directory

--- server.js
+++ server.js
@@ -66,6 +66,10 @@ app.get('/download', function(req, res){
     res.sendFile(path.join(__dirname+'/public/Terminal.zip'));
 });
 
+app.get('/directory', function(req, res){
+    res.sendFile(path.join(__dirname+'/public'));
+});
+
 app.get('/reset', function(req, res){
     const objectives = []
     const allobjectives = JSON.parse(fs.readFileSync('./data/objectives.json'))
