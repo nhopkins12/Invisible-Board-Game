dd7a986605113864c405ab7ecb1be487f3d04cf7
nhopkins12 <62952979+nhopkins12@users.noreply.github.com>
2024-09-26 10:07:44 -0400

    nope

--- boardmake.js
+++ boardmake.js
@@ -514,12 +514,12 @@ var {Spot, Good, Bad, Shop, Teleport, Start, Player} = require("./objects")
                 
                 if (node[direction] !== edge.id) {
                     console.error(`Error: Node ${node.id} should have a ${direction} connection to ${edge.id}.`);
-                    reset()
+                    // reset()
                 }
                 const edgeNode = spots.find(n => n.id === edge.id);
                 if (edgeNode[oppositeDirection] !== node.id) {
                     console.error(`Error: Node ${edge.id} should have a ${oppositeDirection} connection to ${node.id}.`);
-                    reset()
+                    // reset()
                 }
             });
         });
